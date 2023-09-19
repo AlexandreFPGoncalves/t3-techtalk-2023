e67ee921095c2acd1d8a206c542728c8d6b30a8c
Diogo Nogueira <diogoclanogueira@gmail.com>
2023-09-20 00:40:52 +0100

    🚧 Responsive Navbar

---
+++ src/components/navbar/closeIcon.tsx
@@ -0,0 +1,18 @@
+export const CloseIcon = (props: React.ComponentProps<"svg">) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M18 6 6 18" />
+      <path d="m6 6 12 12" />
+    </svg>
+  );
+};
